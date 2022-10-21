e08bccf4efceed80a59499b864346970624c9f8c
FaithAnn Muthoni Njoroge <faithannenjoroge@gmail.com>
2022-10-21 00:39:45 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,23 @@
 #include "main.h"
 
 /**
- * print_alphabet print - all the alphabets in lowercse
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
  */
 
-void print_alphabet(void);
-^ ^
-	char letter;
+void print_alphabet(void)
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-	{
-		_putchar(letter);
-	}
-	_putchar('\n');
+{
+
+		char ch;
+
+		ch = 'a';
+
+		while (ch <= 'z')
+
+		_putchar(ch);
+
+			ch++
+
+		_putchar('\n');
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,24 @@
 #define MAIN_H
 int _putchar (char c);
 void print_alphabet(void);
+ilphabet_x10(void);
 
+int _islower(int c);
+
+int _isalpha(int c);
+
+int print_sign(int n);
+
+int _abs(int);
+
+int print_last_digit(int);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+11 int add(int, int);
+ 
+ 12 void print_to_98(int n);
+  
 #endif
