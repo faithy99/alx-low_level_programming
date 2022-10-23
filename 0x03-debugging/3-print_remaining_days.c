ae49485fd0a246a8fbea7499e595b1c2046b1f78
FaithAnn Muthoni Njoroge <faithannenjoroge@gmail.com>
2022-10-23 04:20:25 -0700

    3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,7 @@
 
 void print_remaining_days(int month, int  day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 100 == 0) && (year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -32,4 +32,4 @@ void print_remaining_days(int month, int  day, int year)
 			printf("Remaining days: %d\n", 365 - day);
 		}
 	}
-}	
+}
