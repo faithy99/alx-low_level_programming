775a9ceae3ce58e59262dbcbc715a8f32732f010
FaithAnn Muthoni Njoroge <faithannenjoroge@gmail.com>
2022-10-25 06:11:51 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers from 0 to 9
+ * print_numbers - prints the numbers from 0 to 9,
  * followed by a new line
- * Return: void
+ * Return: 0
  */
-
 void print_numbers(void)
 {
 	int i;
@@ -16,5 +15,6 @@ void print_numbers(void)
 	_putchar(i + '0');
 	i++;
 	}
+
 	_putchar('\n');
 }
