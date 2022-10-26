71017ad7d7290dd370c2f81beeb44db44fd2e944
FaithAnn Muthoni Njoroge <faithannenjoroge@gmail.com>
2022-10-25 17:29:28 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaps the values of two integer
+ * @a: An integer to swap
+ * @b: Another integer to swap
+ * Return: coid
+ */
+
+void swap_int(int *a, int *b)
+{
+	int aux;
+
+	aux = *a;
+	*a = *b;
+	*b = aux;
+}
