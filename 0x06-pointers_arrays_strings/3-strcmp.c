5c873271da9eaad1ce4f324c213ca116857c8605
FaithAnn Muthoni Njoroge <faithannenjoroge@gmail.com>
2022-10-29 20:55:30 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,7 +4,7 @@
  * _strcmp - compares two strings values
  * @s1: string 1
  * @s2: string 2
- *  Return: the difference in s1 and s2
+ * Return: the difference between s1 and s2
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -13,9 +13,9 @@ int _strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-	if (s1[i] != s2[i])
-	{
-	return (s1[i] - s2[i]);
-	}
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
 	}
 }
