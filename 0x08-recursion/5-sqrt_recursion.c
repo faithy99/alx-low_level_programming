e37b210501d960f35ff9c17f3bbd91a5d80c1c59
FaithAnn Muthoni Njoroge <faithannenjoroge@gmail.com>
2022-11-03 20:01:05 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,23 @@
 #include "main.h"
+/**
+ * helper - function to loop eith new second variable
+ * @n: int arg
+ *  @result: int arg
+ *  Return: sqaure root
+ */
+int helper(int n, int result)
+{
+	if (result * result == n)
+	{
+		return (result);
+	}
+	else if (result * result > n)
+	{
+		return (-1);
+	}
+	else
+		return (helper(n, result + 1));
+}
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
